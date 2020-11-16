34a2623116da6216af3e49de6bc183cdb33bfffb
VladislavAripov <vlad.aripov.007@gmail.com>
2020-11-17 00:34:01 +0300

    Initial commit

---
+++ OnlineWatchStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineWatchStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
