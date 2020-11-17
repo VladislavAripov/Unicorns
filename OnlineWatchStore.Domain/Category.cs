5a964f7d7ae215c66034e526b2ec0203e38b2696
VladislavAripov <vlad.aripov.007@gmail.com>
2020-11-17 11:35:52 +0300

    Added projects Data and Domain in solution

---
+++ OnlineWatchStore.Domain/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineWatchStore.Domain
+{
+    public class Category
+    {
+        private Category()
+        {
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; protected set; }
+        
+        public string Name { get; protected set; }
+        
+        public IList<Product> Products { get; set; } = new List<Product>();
+    }
+}
