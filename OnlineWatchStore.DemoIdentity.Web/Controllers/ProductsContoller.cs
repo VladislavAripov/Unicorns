6ef60a966179d19760a1e717888f1b65f8f261a5
VladislavAripov <vlad.aripov.007@gmail.com>
2020-12-07 23:31:24 +0300

    Microsoft identity added

---
+++ OnlineWatchStore.DemoIdentity.Web/Controllers/ProductsContoller.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineWatchStore.Data;
+using OnlineWatchStore.Domain;
+using OnlineWatchStore.DemoIdentity.Web.Models;
+
+namespace OnlineWatchStore.DemoIdentity.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ILogger<ProductsController> _logger;
+        private readonly MyAppContext _context;
+
+        public ProductsController(ILogger<ProductsController> logger, MyAppContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ OnlineWatchStore.DemoIdentity.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineWatchStore.DemoIdentity.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public ApplicationDbContext() : base()
+        {
+        }
+    }
+}
